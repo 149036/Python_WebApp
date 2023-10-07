cf4dc6f15e89fc0fd0c37019adb4fc76e665965b
149036 <sqrt2c@gmail.com>
2023-10-07 13:34:53 +0900

    test_request

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def inex():
+    return 'Response Data'
+
+@app.route('/another')
+def another():
+    return 'Another Response'
+
+@app.route('/test_request')
+def test_request():
+    return f'test_request:{request.args.get("dummy")}'
+
